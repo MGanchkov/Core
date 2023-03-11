8de20736f6b5af00f8b8f8884dd0c8c8fae3af22
Аньчков М.Г <mganchkov@mail.ru>
2023-03-11 12:26:58 +0300

    Добавление в Tools класс Description.
    Класс для добавление описаний к реализации кода.
    Так внесено описание для проеков типа: Tas, Tes, Tis.

--- Core/Tools.cs
+++ Core/Tools.cs
@@ -6,6 +6,11 @@ namespace Core
 {
     public class Tools
     {
-
+        public static class Description
+        {
+            public static string Tas { get; } = ".Net Standart 2.0";
+            public static string Tes { get; } = ".Net Framework";
+            public static string Tis { get; } = ".Net 6";
+        }
     }
 }
